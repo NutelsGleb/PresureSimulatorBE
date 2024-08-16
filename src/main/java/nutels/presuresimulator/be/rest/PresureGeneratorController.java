21a029197ea1b167ff8ed0cca223eaf9a67c830c
Gleb <gleb.nutels@softec.cz>
2024-08-16 16:23:40 +0200

    swag generator

--- src/main/java/nutels/presuresimulator/be/rest/PresureGeneratorController.java
+++ src/main/java/nutels/presuresimulator/be/rest/PresureGeneratorController.java
@@ -1,5 +1,7 @@
 package nutels.presuresimulator.be.rest;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import nutels.presuresimulator.be.endpionts.APIv1;
 import nutels.presuresimulator.be.models.GeneratedPresure;
 import nutels.presuresimulator.be.service.GeneratedPresureService;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@Tag(name = "Generator controller")
 public class PresureGeneratorController {
 
     private final ApplicationContext context;
@@ -26,6 +29,7 @@ public class PresureGeneratorController {
 
     @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping(value = APIv1.API_URL_PRESURE_GENERATOR, method = RequestMethod.GET)
+    @Operation
     public HttpEntity <GeneratedPresure> getGeneratedPresure(){
 
         GeneratedPresure result = context.getBean(GeneratedPresure.class);
